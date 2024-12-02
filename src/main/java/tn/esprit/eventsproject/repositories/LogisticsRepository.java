6a0a6bdc4b434456bfba9036abb5cb20a5ad690d
YacineHantous <hantousyacin@gmail.com>
2024-12-02 17:30:51 +0100

    commit correction date

--- src/main/java/tn/esprit/eventsproject/repositories/LogisticsRepository.java
+++ src/main/java/tn/esprit/eventsproject/repositories/LogisticsRepository.java
@@ -3,6 +3,7 @@ package tn.esprit.eventsproject.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.eventsproject.entities.Logistics;
 
+import java.time.LocalDate;  // Add this import statement
 import java.util.List;
 
 public interface LogisticsRepository extends JpaRepository<Logistics, Long> {
